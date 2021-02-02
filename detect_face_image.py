7f4c41a1f1075be1377aae2a2ea4d2f5086d8ec8
Horsefactor <36157836+Horsefactor@users.noreply.github.com>
2021-02-02 22:33:35 +0100

    add detect face

---
+++ detect_face_image.py
@@ -0,0 +1,28 @@
+import cv2
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+# Read the input image
+img = cv2.imread('test.jpg')
+
+# Convert into grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Detect faces
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+# Draw rectangle around the faces
+for (x, y, w, h) in faces:
+    img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    roi_gray = gray[y:y + h, x:x + w]
+    roi_color = img[y:y + h, x:x + w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for (ex, ey, ew, eh) in eyes:
+        cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+# Display the output
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
